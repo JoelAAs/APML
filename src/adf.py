aa7dedcbd50d46ea57400bdcba5acc8dc86e8cb4
Simon Barton <simon.barton@gmx.net>
2023-10-06 11:15:44 +0200

    Time decay.

--- src/adf.py
+++ src/adf.py
@@ -4,23 +4,31 @@ import pandas as pd
 # Assumed Density Filtering
 def ADF(nPlayers:int, results:np.array,
         mu0, sigma0, Sigma_t,
-        predict:callable, update:callable):
+        predict:callable, update:callable,
+        decay:callable = None):
     
-    playerSkills = np.array([[mu0, sigma0, 0]] * nPlayers, dtype=np.float32)
+    playerSkills = np.array([[mu0, sigma0, 0, -1]] * nPlayers, dtype=np.float32)
 
     i,nCorrect = 0,0
     for row in results:
-        p1, p2, y = row
-        mu1, sigma1, nMatches1 = playerSkills[p1]
-        mu2, sigma2, nMatches2 = playerSkills[p2]
+        time, p1, p2, y = row
+        mu1, sigma1, nMatches1, last1 = playerSkills[p1]
+        mu2, sigma2, nMatches2, last2 = playerSkills[p2]
        
         # Predict the outcome, count the hits
         nCorrect += predict(mu1, mu2, sigma1, sigma2, Sigma_t)==y
 
+        # Increate the standard deviation based on the time since their last game
+        if decay != None:
+            if last1 != -1:
+                sigma1 = decay(sigma1,time-last1)
+            if last2 != -1:
+                sigma2 = decay(sigma2,time-last2)
+        
         # Bayesian update
         mu1,sigma1, mu2,sigma2 = update(mu1,sigma1, mu2,sigma2, y, Sigma_t)
-        playerSkills[p1,:] = [mu1, sigma1, nMatches1+1]
-        playerSkills[p2,:] = [mu2, sigma2, nMatches2+1]
+        playerSkills[p1,:] = [mu1, sigma1, nMatches1+1, time]
+        playerSkills[p2,:] = [mu2, sigma2, nMatches2+1, time]
         
         i += 1
         if i%10 == 0:
@@ -30,21 +38,22 @@ def ADF(nPlayers:int, results:np.array,
 
 # ADF on pandas dataframe
 def ADFdf(results_df, mu0, sigma0, Sigma_t,
-          player1Column, player2Column, getWinner:callable,
-          predict:callable, update:callable, shuffle:bool):
+          timeColumn:str, player1Column:str, player2Column:str, getWinner:callable,
+          predict:callable, update:callable, shuffle:bool, decay:callable = None):
     
     # Assign numbers to the players
     players = pd.concat([results_df[player1Column], results_df[player2Column]]).unique()
     playerIDs = {players[i]:i for i in range(len(players))}
     
     # Convert to numpy array
-    results = np.zeros((results_df.shape[0],3),dtype=np.int32) # p1,p2,result
+    results = np.zeros((results_df.shape[0],4),dtype=np.int32) # time,p1,p2,result
     nDecisive = 0
     for _, row in results_df.iterrows():
         y = getWinner(row)
         if y == 0:
             continue
-        results[nDecisive,:] = np.array([playerIDs[row[player1Column]],
+        results[nDecisive,:] = np.array([row[timeColumn],
+                                         playerIDs[row[player1Column]],
                                          playerIDs[row[player2Column]],
                                          y])
         nDecisive += 1
@@ -54,5 +63,5 @@ def ADFdf(results_df, mu0, sigma0, Sigma_t,
         np.random.shuffle(results)
 
     playerSkills, accuracy = ADF(len(players), results,
-                                 mu0, sigma0, Sigma_t, predict, update)
+                                 mu0, sigma0, Sigma_t, predict, update, decay)
     return players, playerSkills, accuracy
--- src/runme.py
+++ src/runme.py
@@ -10,7 +10,7 @@ from Moment_matching import *
 
 # %%
 
-# Q4 - Run a single match
+# Q.4 - Run a single match
 def singleMatch():
 
     # Choose hyper parameters
@@ -50,11 +50,6 @@ def singleMatch():
 
 singleMatch()
 
-# %%
-
-# Predicts y
-def predict(mu1, mu2, sigma1, sigma2, Sigma_t):
-    return round(Py_s1s2(mu1, mu2, sigma1, sigma2, Sigma_t))*2-1 
 
 # %%
 
@@ -121,8 +116,8 @@ momentMatchingVsGibbs()
 
 # %%
 
-# Q.9, Q.10
-def rankTennisTeams():
+# Q.9
+def rankTennisPlayers():
     # Load dataframe from file
     tennis_df = pd.read_csv("../data/tennis.csv", sep=",")
 
@@ -154,4 +149,53 @@ def rankTennisTeams():
                     floatfmt=[".2f",".2f",".2f",".2f",".0f"],
                     tablefmt="latex_raw"))
 
-rankTennisTeams()
+rankTennisPlayers()
+
+
+# %%
+
+# Q.10
+def trackTennisPlayers():
+    # Load dataframe from file
+    tennis_df = pd.read_csv("../data/tennis2.csv", sep=",")
+
+    # Choose hyper parameters
+    mu0, sigma0 = 25, 25/3
+    Sigma_t = (25/6)**2
+    nSamples = 50
+    nBurn = 5
+
+
+    # Run ADF on the dataframe rows
+    update = createGibbsUpdater(nSamples, nBurn)
+    
+    # Predicts y
+    def predict(mu1, mu2, sigma1, sigma2, Sigma_t):
+        return round(Py_s1s2(mu1, mu2, sigma1, sigma2, Sigma_t))*2-1 
+
+    decayRate = 1/100
+    def decay(sigma,dt):
+        dt = np.abs(dt)*decayRate
+        sigma = sigma0 + (sigma-sigma0)*np.exp(-dt)
+        return sigma
+    
+    teams, skills, accuracy = ADFdf(tennis_df, mu0, sigma0, Sigma_t,
+                                    'tourney_year_id','winner_name','loser_name', lambda row : 1,
+                                    predict, update, False, decay)
+
+    
+    # Tabulate resulting posteriors
+    idx = np.flip(np.argsort(skills[:,0]))
+    skilltable = np.column_stack((1+np.arange(len(teams)), teams[idx], skills[idx]))
+    print(tabulate(skilltable,
+                    headers=["Rank", "Team", "mu", "sigma", "Games"]))
+   
+    print(f"Prediction accuray: {accuracy}")
+
+
+    print(tabulate(skilltable,
+                    headers=["Rank", "Team", "mu", "sigma", "Games"],
+                    floatfmt=[".2f",".2f",".2f",".2f",".0f"],
+                    tablefmt="latex_raw"))
+
+trackTennisPlayers()
