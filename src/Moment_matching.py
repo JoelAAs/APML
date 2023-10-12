Fixed Gauss division and switched to variance.

src/Moment_matching.py
@@ -6,8 +6,6 @@ Created on Wed Oct  4 15:55:55 2023
 """
 import numpy as np
 import scipy.stats
-from scipy.stats import truncnorm
-import matplotlib.pyplot as plt
 
 def py(mu, sigma, y):
     t_norm = scipy.stats.norm(loc=mu, scale=sigma)
@@ -45,75 +43,61 @@ def pt_y(mu, sigma, y):
     )
 
 
-def gaussianMultiplication(m1, s1, m2, s2):
-    v1 = s1 ** 2
-    v2 = s2 ** 2
+def gaussianMultiplication(m1, v1, m2, v2):
     v = 1 / (1 / v1 + 1 / v2)
     m = (m1 / v1 + m2 / v2) * v
-    return m, np.sqrt(v)
+    return m, v
 
 
-def gaussianDivision(m1, s1, m2, s2):
-    m, v = gaussianMultiplication(m1, s1, m2, -s2)
-    return m, np.sqrt(v)
+def gaussianDivision(m1, v1, m2, v2):
+    return gaussianMultiplication(m1, v1, m2, -v2)
 
 
 def approx_q(mu_c2t, sigma_c2t, y):
     trunc_q = pt_y(mu_c2t, sigma_c2t, y)
     mu_q = trunc_q.mean()
-    sigma_q = np.sqrt(trunc_q.var())
-    return mu_q, sigma_q, trunc_q
+    var_q = trunc_q.var()
+    return mu_q, var_q, trunc_q
 
 
-def step_t2c(mu_1, sigma_1, mu_2, sigma_2, sigma_t, y):
+def step_t2c(mu_1, var_1, mu_2, var_2, var_t, y):
     mu_c2t = mu_1 - mu_2
-    sigma_c2t = np.sqrt(sigma_1 ** 2 + sigma_2 ** 2 + sigma_t ** 2)
-    mu_q, sigma_q, trunc_q = approx_q(mu_c2t, sigma_c2t, y)
-    mu_t2c, sigma_t2c = gaussianDivision(mu_q, sigma_q, mu_c2t, sigma_c2t)
+    var_c2t = var_1 + var_2 + var_t
+    mu_q, var_q, trunc_q = approx_q(mu_c2t, np.sqrt(var_c2t), y)
+    mu_t2c, var_t2c = gaussianDivision(mu_q, var_q, mu_c2t, var_c2t)
 
-    return mu_t2c, sigma_t2c, trunc_q
+    return mu_t2c, var_t2c, trunc_q
 
 
-def ps1_y(mu_1, sigma_1, mu_2, sigma_2, sigma_t, y):
+def ps1_y(mu_1, var_1, mu_2, var_2, var_t, y):
     """
     :return:
-    mu, sigma of player 2 given y
+    mu, var of player 2 given y
     """
-    mu_t2c, sigma_t2c, _ = step_t2c(mu_1, sigma_1, mu_2, sigma_2, sigma_t, y)
+    mu_t2c, var_t2c, _ = step_t2c(mu_1, var_1, mu_2, var_2, var_t, y)
     mu_c2s1 = mu_t2c + mu_2
-    sigma_c2s1 = np.sqrt(sigma_t ** 2 + sigma_2 ** 2 + sigma_t2c ** 2)
-    mu_s1y, sigma_s1y = gaussianMultiplication(mu_c2s1, sigma_c2s1, mu_1, sigma_1)
-    return mu_s1y, sigma_s1y
+    var_c2s1 = var_t + var_2 + var_t2c
+    mu_s1y, var_s1y = gaussianMultiplication(mu_c2s1, var_c2s1, mu_1, var_1)
+    return mu_s1y, var_s1y
 
 
-def ps2_y(mu_1, sigma_1, mu_2, sigma_2, sigma_t, y):
+def ps2_y(mu_1, var_1, mu_2, var_2, var_t, y):
     """
     :return:
-    mu, sigma of player 2 given y
+    mu, var of player 2 given y
     """
-    mu_t2c, sigma_t2c, _ = step_t2c(mu_1, sigma_1, mu_2, sigma_2, sigma_t, y)
+    mu_t2c, var_t2c, _ = step_t2c(mu_1, var_1, mu_2, var_2, var_t, y)
     mu_c2s2 = mu_1 - mu_t2c
-    sigma_c2s2 = np.sqrt(sigma_t ** 2 + sigma_1 ** 2 + sigma_t2c ** 2)
-    mu_s2y, sigma_s2y = gaussianMultiplication(mu_c2s2, sigma_c2s2, mu_2, sigma_2)
-    return mu_s2y, sigma_s2y
+    var_c2s2 = var_t + var_1 + var_t2c
+    mu_s2y, var_s2y = gaussianMultiplication(mu_c2s2, var_c2s2, mu_2, var_2)
+    return mu_s2y, var_s2y
 
 
 def createMomentMatching():
-    def updateMomentMatching(mu1, sigma1, mu2, sigma2, y, Sigma_t):
-        sigma_t = np.sqrt(Sigma_t)
-        mu1n, sigma1n = ps1_y(mu1, sigma1, mu2, sigma2, sigma_t, y)
-        mu2n, sigma2n = ps2_y(mu1, sigma1, mu2, sigma2, sigma_t, y)
-        return mu1n, sigma1n, mu2n, sigma2n
+    def updateMomentMatching(mu1, var1, mu2, var2, var_t, y):
+        mu1n, var1n = ps1_y(mu1, var1, mu2, var2, var_t, y)
+        mu2n, var2n = ps2_y(mu1, var1, mu2, var2, var_t, y)
+        return mu1n, var1n, mu2n, var2n
 
     return updateMomentMatching
 
-
-def test():
-    mu0, sigma0 = 25, 25 / 3
-    sigma_t = 25 / 6
-    y = -1
-
-    x = np.linspace(0, 40, 200)
-    plt.plot(x, scipy.stats.norm(mu1, s1).pdf(x), "r")
-    plt.plot(x, scipy.stats.norm(mu2, s2).pdf(x), "g")
-    plt.show()

src/adf.py
@@ -3,36 +3,36 @@ import pandas as pd
 
 # Assumed Density Filtering
 def ADF(nPlayers:int, results:np.array,
-        mu0, sigma0, Sigma_t,
+        mu0, var0, var_t,
         predict:callable, update:callable,
         decay:callable = None):
     
-    playerSkills = np.array([[mu0, sigma0, 0, -1]] * nPlayers, dtype=np.float32)
+    playerSkills = np.array([[mu0, var0, 0, -1]] * nPlayers, dtype=np.float32)
     history = [[] for _ in range(nPlayers)]
 
     i,nCorrect = 0,0
     for row in results:
         time, p1, p2, y = row
-        mu1, sigma1, nMatches1, last1 = playerSkills[p1]
-        mu2, sigma2, nMatches2, last2 = playerSkills[p2]
-       
+        mu1, var1, nMatches1, last1 = playerSkills[p1]
+        mu2, var2, nMatches2, last2 = playerSkills[p2]
+
         # Predict the outcome, count the hits
-        nCorrect += predict(mu1, mu2, sigma1, sigma2, Sigma_t)==y
+        nCorrect += predict(mu1, var1, mu2, var2, var_t)==y
 
         # Increate the standard deviation based on the time since their last game
         if decay != None:
             if last1 != -1:
-                sigma1 = decay(sigma1,time-last1)
+                var1 = decay(var1,time-last1)
             if last2 != -1:
-                sigma2 = decay(sigma2,time-last2)
+                var2 = decay(var2,time-last2)
         
         # Bayesian update
-        mu1,sigma1, mu2,sigma2 = update(mu1,sigma1, mu2,sigma2, y, Sigma_t)
-        playerSkills[p1,:] = [mu1, sigma1, nMatches1+1, time]
-        playerSkills[p2,:] = [mu2, sigma2, nMatches2+1, time]
+        mu1,var1, mu2,var2 = update(mu1,var1, mu2,var2, var_t, y)
+        playerSkills[p1,:] = [mu1, var1, nMatches1+1, time]
+        playerSkills[p2,:] = [mu2, var2, nMatches2+1, time]
         
-        history[p1].append([time, mu1, sigma1])
-        history[p2].append([time, mu2, sigma2])
+        history[p1].append([time, mu1, var1])
+        history[p2].append([time, mu2, var2])
 
         i += 1
         if i%10 == 0:
@@ -41,7 +41,7 @@ def ADF(nPlayers:int, results:np.array,
     return playerSkills, nCorrect/i, [np.array(h) for h in history]
 
 # ADF on pandas dataframe
-def ADFdf(results_df, mu0, sigma0, Sigma_t,
+def ADFdf(results_df, mu0, var0, var_t,
           timeColumn:str, player1Column:str, player2Column:str, getWinner:callable,
           predict:callable, update:callable, shuffle:bool, consider_draw = False,
           decay:callable = None):
@@ -68,5 +68,6 @@ def ADFdf(results_df, mu0, sigma0, Sigma_t,
         np.random.shuffle(results)
 
     playerSkills, accuracy, history = ADF(len(players), results,
-                                 mu0, sigma0, Sigma_t, predict, update, decay)
+                                          mu0, var0, var_t,
+                                          predict, update, decay)
     return players, playerSkills, accuracy, history

src/gibbs.py
@@ -5,9 +5,9 @@ import numpy as np
 import numpy.linalg as lg
 
 # P(t|s1,s2,y)
-def Pt_s1s2y(s1, s2, y, Sigma_t):
+def Pt_s1s2y(s1, s2, y, var_t):
     mean = s1 - s2
-    sigma_t = np.sqrt(Sigma_t)
+    sigma_t = np.sqrt(var_t)
     if y > 0:
         lim1 = -mean / sigma_t
         lim2 = np.inf
@@ -25,36 +25,35 @@ def Pt_s1s2y(s1, s2, y, Sigma_t):
     return t_new
 
 # P(s1,s2|t)
-def Ps1s2_t(t, mu_1, mu_2, sigma_1, sigma_2, Sigma_t):
-    cov_s = np.array([[sigma_1**2, 0], [0, sigma_2**2]])
+def Ps1s2_t(t, mu_1, var_1, mu_2, var_2, var_t):
+    cov_s = np.array([[var_1, 0], [0, var_2]])
     A = np.array([[1, -1]])
-    Sigma_s1s2_t = lg.inv(lg.inv(cov_s) + A.T@A / Sigma_t)
+    Sigma_s1s2_t = lg.inv(lg.inv(cov_s) + A.T@A / var_t)
     mu_s1s2_t = Sigma_s1s2_t @ (
             lg.inv(cov_s) @ np.array([mu_1, mu_2]).T +
-            A[0] / Sigma_t * t
-    )
+            A[0] / var_t * t )
     s1,s2 = np.random.multivariate_normal(mu_s1s2_t,
-                                         Sigma_s1s2_t,
-                                         1)[0]
+                                          Sigma_s1s2_t,
+                                          1)[0]
     return s1,s2
 
 # P(y=1)
-def Py_s1s2(mu1, mu2, sigma1, sigma2, Sigma_t):
+def Py_s1s2(mu1, var1, mu2, var2, var_t):
     mu = mu1 - mu2
-    sigma = Sigma_t + (sigma1**2 + sigma2**2)
-    return erfc(-mu/np.sqrt(2)/sigma)/2 # P(t>0), t~N(mu,sigma^2)
+    var = var_t + var1 + var2
+    return erfc(-mu/np.sqrt(2)/var)/2 # P(t>0), t~N(mu,var)
 
 
 # Sample from the posterior P(s1,s2|t,y)
-def sample(y, mu_1, mu_2, sigma_1, sigma_2, Sigma_t, nSamples):
+def sample(mu_1, var_1, mu_2, var_2, var_t, y, nSamples):
     S_1 = np.zeros(nSamples)
     S_2 = np.zeros(nSamples)
     T = np.zeros(nSamples)
 
     s_1, s_2 = 0, 0
     for k in range(nSamples):
-        t = Pt_s1s2y(s_1, s_2, y, Sigma_t)
-        s_1, s_2 = Ps1s2_t(t, mu_1, mu_2, sigma_1, sigma_2, Sigma_t)
+        t = Pt_s1s2y(s_1, s_2, y, var_t)
+        s_1, s_2 = Ps1s2_t(t, mu_1, var_1, mu_2, var_2, var_t)
         S_1[k] = s_1
         S_2[k] = s_2
         T[k] = t
@@ -63,24 +62,22 @@ def sample(y, mu_1, mu_2, sigma_1, sigma_2, Sigma_t, nSamples):
 
 # Finds the underlying normal distribution
 def gaussian_approx(s_vec):
-    s_var = np.sqrt(np.var(s_vec))
+    s_var = np.var(s_vec)
     s_mean = np.mean(s_vec)
-    return norm(loc=s_mean, scale=s_var), s_mean, s_var
+    return norm(loc=s_mean, scale=np.sqrt(s_var)), s_mean, s_var
 
 
 # Creates a callable Bayesian updater that uses Gibbs sampling
 def createGibbsUpdater(nSamples, nBurn):
-    def updateGibbs(mu1,sigma1, mu2,sigma2, y, Sigma_t):
+    def updateGibbs(mu1,var1, mu2,var2, var_t, y):
         # Sample from the posterior
-        s1s, s2s, _ = sample(y,
-                            mu1, mu2,
-                            sigma1, sigma2,
-                            Sigma_t,
-                            nSamples)
+        s1s, s2s, _ = sample(mu1, var1,
+                             mu2, var2,
+                             var_t, y, nSamples)
 
         # Estimate the new parameters from the normal distributions
-        _, mu1, sigma1 = gaussian_approx(s1s[nBurn:])
-        _, mu2, sigma2 = gaussian_approx(s2s[nBurn:])
-        return mu1, sigma1, mu2, sigma2
+        _, mu1, var1 = gaussian_approx(s1s[nBurn:])
+        _, mu2, var2 = gaussian_approx(s2s[nBurn:])
+        return mu1, var1, mu2, var2
     return updateGibbs
 

src/runme.py
@@ -2,11 +2,11 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import scipy
 from tabulate import tabulate
 from gibbs import Py_s1s2, sample, gaussian_approx, createGibbsUpdater
 from adf import ADFdf
-import scipy
-from Moment_matching import *
+from momentMatching import createMomentMatching, ps1_y, ps2_y, py
 
 # %%
 
@@ -15,12 +15,12 @@ def singleMatch():
 
     # Choose hyper parameters
     y = 1
-    mu_1, mu_2 = 25, 25
-    sigma_1, sigma_2 = 25/3, 25/3
-    sigma_t = 1.5
+    mu1, var1 = 25, (25/3)**2
+    mu2, var2 = 25, (25/3)**2
+    var_t = (25/6)**2
     N_samples = 1000
     n_burn = 5
-    S_1, S_2, T = sample(y, mu_1, mu_2, sigma_1, sigma_2, sigma_t, N_samples)
+    S_1, S_2, T = sample(mu1, var1, mu2, var2, var_t, y, N_samples)
 
     # Q4.1 burn-in is about 5 values
     gen = np.linspace(0, N_samples - 1, N_samples)
@@ -34,7 +34,7 @@ def singleMatch():
     s1_approx, _, _ = gaussian_approx(S_1[n_burn:])
     s2_approx, _, _ = gaussian_approx(S_2[n_burn:])
     
-    x = np.linspace(0, 40, 200)
+    x = np.linspace(0, 60, 200)
     p_s1 = s1_approx.pdf(x)
     p_s2 = s2_approx.pdf(x)
 
@@ -45,7 +45,7 @@ def singleMatch():
     plt.hist(S_2[n_burn:], bins=bins, color="g", density=True)
     plt.plot(x, p_s2, color="darkgreen")
     plt.hist(T[n_burn:], bins=bins, color="b", density=True)
-    plt.legend(["S1", "P1", "S2", "P2", "T"])
+    plt.legend(["s1", "p1", "s2", "p2", "t"])
     plt.show()
 
 singleMatch()
@@ -58,8 +58,8 @@ def rankFootballTeams():
     seriesA_df = pd.read_csv("../data/SerieA.csv", sep=",")
 
     # Choose hyper parameters
-    mu0, sigma0 = 25, 25/3
-    Sigma_t = (25/6)**2
+    mu0, var0 = 25, (25/3)**2
+    var_t = (25/6)**2
     nSamples = 100
     nBurn = 5
 
@@ -67,20 +67,23 @@ def rankFootballTeams():
     update = createGibbsUpdater(nSamples, nBurn)
 
      # Predicts y
-    def predict(mu1, mu2, sigma1, sigma2, Sigma_t):
-        return round(Py_s1s2(mu1, mu2, sigma1, sigma2, Sigma_t))*2-1 
+    def predict(mu1, var1, mu2, var2, var_t):
+        return round(Py_s1s2(mu1, var1, mu2, var2, var_t))*2-1
     
-    teams, skills, accuracy, _ = ADFdf(seriesA_df, mu0, sigma0, Sigma_t,
-                                    '','team1','team2', lambda row : np.sign(row["score1"] - row["score2"]),
-                                    predict, update, False)
+    teams, skills, accuracy, _ = ADFdf(seriesA_df, mu0, var0, var_t,
+                                       '','team1','team2', lambda row : np.sign(row["score1"] - row["score2"]),
+                                       predict, update, False)
 
-    
+    skills[:,2] = np.sqrt(skills[:,2])
+
     # Tabulate resulting posteriors
     idx = np.flip(np.argsort(skills[:,0]))
-    skilltable = np.column_stack((1+np.arange(len(teams)), teams[idx], skills[idx]))
+    skilltable = np.column_stack((1+np.arange(len(teams)), teams[idx], skills[idx,:3]))
     print(tabulate(skilltable,
-                    headers=["Rank", "Team", "mu", "sigma", "Games"]))
-    print(f"Prediction accuray: {accuracy}")
+                    headers=["Rank", "Team", "mu", "sigma", "Games"],
+                    floatfmt=[".0f",".2f",".2f",".2f",".0f"],
+                    tablefmt="latex_raw"))
+    print(f"Prediction accuracy: {accuracy}")
 
 rankFootballTeams()
 
@@ -89,26 +92,31 @@ rankFootballTeams()
 
 # Q.8
 def momentMatchingVsGibbs():
-    mu0, sigma0 = 25, 25 / 3
-    sigma_t = 25 / 6
+    mu0, var0 = 25, (25 / 3)**2
+    var_t = (25 / 6)**2
+
     y = 1
-    N_samples = 1000
+    nSamples = 1000
 
     # Gibbs
-    S_1, S_2, _ = sample(y, mu0, mu0, sigma0, sigma0, sigma_t, N_samples)
+    S_1, S_2, _ = sample(mu0, var0, mu0, var0, var_t, y, nSamples)
 
     # Moment matching
-    mu1mm, s1mm = ps1_y(mu0, sigma0, mu0, sigma0, sigma_t, y)
-    mu2mm, s2mm = ps2_y(mu0, sigma0, mu0, sigma0, sigma_t, y)
+    mu1mm, var1mm = ps1_y(mu0, var0, mu0, var0, var_t, y)
+    mu2mm, var2mm = ps2_y(mu0, var0, mu0, var0, var_t, y)
 
-    bins = int(np.sqrt(N_samples))
+    sigma1mm, sigma2mm = np.sqrt(var1mm), np.sqrt(var2mm)
+
+    print(mu1mm, sigma1mm)
+
+    bins = int(np.sqrt(nSamples))
     x = np.linspace(0, 50, 200)
-    plt.hist(S_1, bins=bins, color="darkred", density=True)
-    plt.plot(x, scipy.stats.norm(mu1mm, s1mm).pdf(x), "r")
-    plt.hist(S_2, bins=bins, color="darkgreen", density=True)
-    plt.plot(x, scipy.stats.norm(mu2mm, s2mm).pdf(x), "g")
-    plt.legend(["P(S1|y) MM",  "P(S2|y) MM", "P(S1|y) gibbs", "P(S2|y) gibbs"])
-    plt.title("Moment Matching vs Gibbs sampling of S posterior")
+    plt.hist(S_1, bins=bins, color="darkred", density=True, alpha=0.6)
+    plt.plot(x, scipy.stats.norm(mu1mm, sigma1mm).pdf(x), "r")
+    plt.hist(S_2, bins=bins, color="darkgreen", density=True, alpha=0.6)
+    plt.plot(x, scipy.stats.norm(mu2mm, sigma2mm).pdf(x), "g")
+    plt.legend(["p(s1|y) MM", "p(s2|y) MM", "p(s1|y) Gibbs", "p(s2|y) Gibbs"])
+    plt.title("Moment Matching vs. Gibbs sampling of s posterior.")
     plt.show()
 
 momentMatchingVsGibbs()
@@ -121,17 +129,17 @@ def rankFootballTeamsDraw():
     seriesA_df = pd.read_csv("../data/SerieA.csv", sep=",")
 
     # Choose hyper parameters
-    mu0, sigma0 = 25, 25/3
-    Sigma_t = (25/6)**2
+    mu0, var0 = 25, (25/3)**2
+    var_t = (25/6)**2
     
     # -------------------------
     # Without draws
     update = createMomentMatching()
 
-    def predict(mu1, mu2, sigma1, sigma2, Sigma_t):
-        return round(Py_s1s2(mu1, mu2, sigma1, sigma2, Sigma_t))*2-1 
+    def predict(mu1, var1, mu2, var2, var_t):
+        return round(Py_s1s2(mu1, var1, mu2, var2, var_t))*2-1
     
-    _, _, accuracy, _ = ADFdf(seriesA_df, mu0, sigma0, Sigma_t,
+    _, _, accuracy, _ = ADFdf(seriesA_df, mu0, var0, var_t,
                               '','team1','team2', lambda row : np.sign(row["score1"] - row["score2"]),
                               predict, update, False)
 
@@ -140,29 +148,32 @@ def rankFootballTeamsDraw():
     # With draws
     update = createMomentMatching()
 
-    def predict_draws(mu1, mu2, sigma1, sigma2, Sigma_t):
+    def predict_draws(mu1, var1, mu2, var2, var_t):
         mu = mu1-mu2
-        sigma = np.sqrt(sigma1**2 + sigma2**2 + Sigma_t)
+        sigma = np.sqrt(var1 + var2 + var_t)
 
         values = [py(mu, sigma, y) for y in range(-1, 2)]
         predicted_value = np.argmax(values) - 1
         return predicted_value
 
-    teams, skills, accuracy_draw, _ = ADFdf(seriesA_df, mu0, sigma0, Sigma_t,
-                                   '','team1', 'team2', lambda row: np.sign(row["score1"] - row["score2"]),
-                                   predict_draws, update, False, consider_draw=True)
+    teams, skills, accuracy_draw, _ = ADFdf(seriesA_df, mu0, var0, var_t,
+                                            '','team1', 'team2',
+                                            lambda row: np.sign(row["score1"] - row["score2"]),
+                                            predict_draws, update, False, consider_draw=True)
+
+    skills[:,2] = np.sqrt(skills[:,2])
 
     # -------------------------
     # Tabulate resulting posteriors
     idx = np.flip(np.argsort(skills[:,0]))
-    skilltable = np.column_stack((1+np.arange(len(teams)), teams[idx], skills[idx]))
+    skilltable = np.column_stack((1+np.arange(len(teams)), teams[idx], skills[idx,:3]))
     print(tabulate(skilltable,
                     headers=["Rank", "Team", "mu", "sigma", "Games"],
-                    floatfmt=[".2f",".2f",".2f",".2f",".0f"],
+                    floatfmt=[".0f",".2f",".2f",".2f",".0f"],
                     tablefmt="latex_raw"))
 
-    print(f"Prediction accuray without draws: {accuracy}")
-    print(f"Prediction accuray with draws: {accuracy_draw}")
+    print(f"Prediction accuracy without draws: {accuracy}")
+    print(f"Prediction accuracy with draws: {accuracy_draw}")
 
 
 rankFootballTeamsDraw()
@@ -179,44 +190,43 @@ def trackTennisPlayers():
     tennis_df = pd.read_csv("../data/tennis2.csv", sep=",")
 
     # Choose hyper parameters
-    mu0, sigma0 = 25, 25/3
-    Sigma_t = (25/6)**2
+    mu0, var0 = 25, (25/3)**2
+    var_t = (25/6)**2
 
     # Choose update method
     update = createGibbsUpdater(nSamples=50, nBurn=5)
     update = createMomentMatching()
 
     # Predicts y
-    def predict(mu1, mu2, sigma1, sigma2, Sigma_t):
-        return round(Py_s1s2(mu1, mu2, sigma1, sigma2, Sigma_t))*2-1 
+    def predict(mu1, var1, mu2, var2, var_t):
+        return round(Py_s1s2(mu1, var1, mu2, var2, var_t))*2-1 
 
     decayRate = 1/365
-    def decay(sigma,dt):
-        dt = dt*decayRate
-        sigma = sigma0 + (sigma-sigma0)*np.exp(-dt)
-        return sigma
+    def decay(var,dt):
+        return var0 + (var-var0)*np.exp(-dt*decayRate)
 
-    players, skills, accuracy, history = ADFdf(tennis_df, mu0, sigma0, Sigma_t,
-                                    'day','winner_name','loser_name', lambda row : 1,
-                                    predict, update, False, False, decay)
+    players, skills, accuracy, history = ADFdf(tennis_df, mu0, var0, var_t,
+                                               'day','winner_name','loser_name',
+                                               lambda row : 1,
+                                               predict, update, False, False, decay)
 
-
+    skills[:,2] = np.sqrt(skills[:,2])
 
     idx = np.flip(np.argsort(skills[:,0]))
-    skilltable = np.column_stack((1+np.arange(len(players)), players[idx], skills[idx]))
+    skilltable = np.column_stack((1+np.arange(len(players)), players[idx], skills[idx,:3]))
     skilltable = skilltable[:20]
 
     print(tabulate(skilltable,
                     headers=["Rank", "Team", "mu", "sigma", "Games"],
-                    floatfmt=[".2f",".2f",".2f",".2f",".0f"],
+                    floatfmt=[".0f",".2f",".2f",".2f",".0f"],
                     tablefmt="latex_raw"))
-    print(f"Prediction accuray: {accuracy}")
+    print(f"Prediction accuracy: {accuracy}")
 
     # Draw history of skill
     for i in idx[:5]:
         xs = history[i][:,0]
         ys = history[i][:,1]
-        errs = history[i][:,2]*1.96 # 95% confidence
+        errs = np.sqrt(history[i][:,2])*3 # 99% confidence
         plt.plot(xs,ys,label=players[i])
         plt.fill_between(xs, ys-errs, ys+errs, alpha=0.5)
         plt.legend()